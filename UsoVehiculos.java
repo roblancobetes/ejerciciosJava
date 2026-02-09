6c29798bddbc5c207c2f6fbbf4112d340cbe21f1
Rodrigo Blanco (Alfa Formación) <rblanco.docente@alfa-formacion.com>
2026-02-09 10:33:42 +0100

    Añadir herencia, polimorfismo y modificador final

--- UsoVehiculos.java
+++ UsoVehiculos.java
@@ -6,7 +6,15 @@ public class UsoVehiculos {
 
         Coche cochePepito = new Coche("E", 1993, "Seat Ibiza", 1000000, false);
 
-        Coche[] listaCoches = {micoche, cochePepito};
+        Furgoneta miFurgoneta = new Furgoneta("D", 2010, "Seat Furgoneta", 50000, false, 2000);
+
+        Coche[] listaCoches = {micoche, cochePepito, miFurgoneta};
+
+        cochePepito.instalarAire();
+
+        miFurgoneta.instalarAire();
+
+        System.out.println("La capacidad de carga es " + ((Furgoneta)listaCoches[2]).getCapacidadCarga());
 
         for (Coche coche : listaCoches) {
 
@@ -16,11 +24,15 @@ public class UsoVehiculos {
 
             else cadena = " y no tiene aire acondicionado";
 
-            System.out.println("Mi coche es un " + coche.getModelo() + ", tiene etiqueta " + 
+            System.out.println("Mi coche, de ID " + coche.getIdCoche() + " es un " + coche.getModelo() + ", tiene etiqueta " + 
             coche.getEtiqueta() + cadena);
+
+            System.out.println("Y estimo un precio de " + coche.precio());
             
         }
-  
+
+        System.out.print("El número total de coches en el garaje es de " + Coche.getNumeroCoches());
+
     }
 
 }
@@ -40,18 +52,39 @@ class Coche {
 
             this.tieneAire = tieneAire;
 
+            Coche.numeroCoches++;
+
+            if (Coche.numeroCoches > Coche.MAX_COCHES) throw new IllegalArgumentException("Número máximo de coches excedido");
+
+            this.idCoche = Coche.numeroCoches;
+
     }
 
+    //Atributos privados de instancias
     private String modelo;
 
     private int numKilometros;
 
     private int fechaMatriculacion;
 
+    private final int idCoche;
+
     private boolean tieneAire;
 
     private String etiqueta;
 
+    public static final int MAX_COCHES = 100;
+
+    //Atributos static
+    private static int numeroCoches = 0;
+
+    public static int getNumeroCoches() {
+
+        return Coche.numeroCoches;
+
+    }
+
+    //Métodos getters y setters
     public String getModelo() {
         return modelo;
     }
@@ -92,5 +125,59 @@ class Coche {
     public void setEtiqueta(String etiqueta) {
         this.etiqueta = etiqueta;
     }
+
+    //Métodos definidos
+    public int precio() {
+
+        int precio = 5000;
+
+        precio -= (int) 10*(2026-this.fechaMatriculacion);
+
+        if (this.tieneAire) precio += 1000;
+        else precio -= 1000;
+
+        if (this.etiqueta.equals("A")||this.etiqueta.equals("ECO")) precio += 1000;
+        else precio -= 1000;
+
+        return precio;
+
+    }
+
+    public void instalarAire() {
+
+        this.tieneAire = true;
+
+    }
+
+    public int getIdCoche() {
+        return idCoche;
+    }
+
+}
+
+class Furgoneta extends Coche {
+
+    //Método constructor de una nueva furgoneta
+    public Furgoneta(String etiqueta, int fechaMatriculacion, String modelo, int numKilometros, boolean tieneAire, int capacidadCarga) {
+
+        //Llama al método constructor de la clase Coche
+        super(etiqueta, fechaMatriculacion, modelo, numKilometros, tieneAire);
+
+        this.capacidadCarga = capacidadCarga;
+
+    }
+
+    private int capacidadCarga;
+
+    public int getCapacidadCarga() {
+        return this.capacidadCarga;
+    }
+
+    public void setCapacidadCarga(int capacidadCarga) {
+        this.capacidadCarga = capacidadCarga;
+    }
+
+
+
 
 }
